a937d61b00a7b5f24f3a486bd8d57525d2046717
Khoi Luc <luckhoi89@gmail.com>
2018-05-26 17:51:40 -0700

    update

--- routes/product.js
+++ routes/product.js
@@ -4,4 +4,12 @@ exports = module.exports = function (req, res){
   var locals = res.locals;
 
   //set locals
+  locals.section = 'store';
+
+  //load Products
+  view.query('products', keystone.list('Product').model().find());
+
+  // Render view
+
+  view.render('products');
 }
